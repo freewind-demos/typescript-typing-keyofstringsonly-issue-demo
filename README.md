a0f9df12cf4eff0626305a47b04bd41ab0d89663
freewind <nowindlee@gmail.com>
2019-04-26 22:48:24 +0800

    demo

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Typing Field With Extra Type Demo
+============================================
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,36 @@
-console.log('Hello');
+type CommonConfig = {
+  statePath: string
+}
+
+type FormElement = ({
+  type: 'checkbox',
+  config: {
+    checked: boolean
+  }
+} | {
+  type: 'input',
+  config: {
+    text: string
+  }
+}) & {
+  config: CommonConfig
+}
+
+const elements: FormElement[] = [
+  {
+    type: 'checkbox',
+    config: {
+      checked: false,
+      statePath: 'aa'
+    }
+  },
+  {
+    type: 'input',
+    config: {
+      text: 'hello',
+      statePath: 'bb'
+    }
+  },
+]
+
+console.log(elements)
